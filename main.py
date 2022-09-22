05477436303417be8e21e4a7082b79faad621390
Hakan <83027391+jroken@users.noreply.github.com>
2022-09-22 23:00:56 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
 from serial import Serial 
 from time import sleep
-import speech_recognition as sr
-import pyttsx3
-engine = pyttsx3.init("sapi5")
-engine.setProperty('rate', 130)
+from gtts import gTTS
+import speech_recognition as spr
+import os
+import random
+from playsound import playsound
+
 ArduinoSerial = Serial('com6',9600)
+
 sleep(2)
-def speak(text):
-    speak(text) 
+
+def speak(string):
+    tts = gTTS(text=string, lang='tr', slow=False)
+    rand = random.randint(1, 30)
+    sayi = 1 + rand
+    file = f"answer{sayi}.mp3"
+    tts.save(file)
+    playsound(file)
+    os.remove(file)
+
 speak('Günaydın Efendim')
 print(ArduinoSerial.readline())
+
 while True:
-    r = sr.Recognizer()
-    with sr.Microphone() as source:
+    r = spr.Recognizer()
+    with spr.Microphone() as source:
         r.adjust_for_ambient_noise(source)
-        print("Arka plan gürültüsü:" + str(r.energy_threshold))
-        ses = r.listen(source, timeout=10, phrase_time_limit=10)
-        veri = r.recognize_google(ses, language='tr-tr')
-        print(veri)
-        
-    if (veri == 'yeşil Işıkları aç'):
+        print("Listening:" + str(r.energy_threshold))
+        voice = r.listen(source)
+        veri = r.recognize_google(voice, language='tr-Tr', show_all=False)
+        print(r.recognize_google(voice, language='tr-Tr', show_all=False))
+
+    if (veri == 'Selam'):
+        speak('Açıyorum canım')
         ArduinoSerial.write(b'g')
-        speak('Open The All Green Light')
+        
 
     if (veri == 'mavi Işıkları aç'):
         ArduinoSerial.write(b'b')
